0d01536e125cbcc831a875d26c4b92fed6eb7756
Adele MA² <thesubjectivesubject@gmail.com>
2017-10-04 09:48:35 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+$(document).ready(function() { 
+
+
+//hover for buttons
+$('a').hover(function() {
+	$(this).css('background-color', 'white');},
+	function() {
+	$(this).css('background-color', '#44d9e6')
+});
+
+$('button').hover(function() {
+	$(this).css('background-color', 'white');},
+	function() {
+	$(this).css('background-color', '#44d9e6')
+});
+
+
+
+
+//animation to the header
+$('.date').hide();
+$('#navbr').hide();
+
+//moving from main page to page with header and stuff
+$('.header').click(function(e) { 
+    $('.header').hide().show('slow');
+    $('.date').show('slow');
+    $('#navbr').show('slow');
+    $('#butt').hide();
+});
+
+
+//overlay for info button
+var $overlay = $('<div id="overlay"></div>'); //jQuery object
+
+$('.learnMore').click (function(e) {
+//To add an overlay
+
+//var $image = $('<img>');
+var $caption = $('<p>Are you Afro-caribbean and wondered why there are not more people like you in tech? Are you under 25 and want to get a feel for the industry but do not know how? We gotchu you! YAC are hosting a FREE 3 day event for you to get a taste of all things #enterprise, tech and #design. In November we will be running our first ever YACATHON. Expert or novice we have something for you.</p>');
+
+// a caption to overlay
+$overlay.append($caption);
+
+$overlay.show();
+
+  // an image to overlay
+//$overlay.append($image);
+
+//add an overlay
+$('body').append($overlay);
+
+})
+
+//when overlay is clicked hide the overlay
+//hide the overlay
+ $overlay.click(function(){
+ 	$overlay.hide();
+ });
+
+
+})
